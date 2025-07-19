485dd8c4c393d110fa253088a91f9f54f2743e1c
VanoStreyPracticum <vanostrey@yandex.ru>
2025-07-19 18:17:12 +0300

    Сделал историю бесконечной и уникальной

--- src/ru/yandex/task_trecker/service/InMemoryHistoryManager.java
+++ src/ru/yandex/task_trecker/service/InMemoryHistoryManager.java
@@ -12,7 +12,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void add(Task task) {
         if (task == null) return;
-        remove(task.getId());  // удалим старое вхождение
+        remove(task.getId());
         linkLast(task);
     }
 
