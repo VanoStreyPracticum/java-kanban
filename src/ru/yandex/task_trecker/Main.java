e62eed88a87128d0abbe66f4ee3e2c2a7e3ada9f
w0wshee <timval2708@yandex.ru>
2025-07-29 14:10:00 +0300

    реализована функциянальность записи задач в файл

--- src/ru/yandex/task_trecker/Main.java
+++ src/ru/yandex/task_trecker/Main.java
@@ -3,7 +3,6 @@ package ru.yandex.task_trecker;
 import ru.yandex.task_trecker.service.*;
 import ru.yandex.task_trecker.task_data.*;
 
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
